0796c3d0f8005a9f4bdf956105b82611718c6dd4
yeasinarafat51 <aloondokar65@gmail.com>
2022-11-10 10:12:02 +0600

    five

--- src/pages/shared/blog/Blog.js
+++ src/pages/shared/blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
             <h1 className='text-orange-600'>Difference between SQL and NoSQL</h1>
             <p>SQL databases are vertically scalable, while NoSQL databases are horizontally scalable. SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores. SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON.</p>
             <h1 className='text-orange-600'>What is JWT, and how does it work?</h1>
-            <p> JWTs are a good way of securely transmitting informati between parties because they can be signed, which means you can be sure that the senders are who they say they are. Additionally, the structure of a JWT allows you to verify that the content hasn't been tampered with.</p>
+            <p> JWTs are a good way of securely transmitting informat between parties because they can be signed, which means you can be sure that the senders are who they say they are. Additionally, the structure of a JWT allows you to verify that the content hasn't been tampered with.</p>
             <h1 className='text-orange-600'>What is the difference between javascript and NodeJS?</h1>
             <p >JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language</p>
             <h1 className='text-orange-600'>How does NodeJS handle multiple requests at the same time?</h1>
