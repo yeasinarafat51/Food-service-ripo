7e7d379e57ad40b063adb51a549fc43a36258dd6
yeasinarafat51 <aloondokar65@gmail.com>
2022-11-10 20:39:46 +0600

    first

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Authprovider from './contexts/authprovider/Authprovider';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider } from 'react-photo-view';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Authprovider>
-    <App />
+   <PhotoProvider>
+   <App />
+   </PhotoProvider>
     
     </Authprovider>
   </React.StrictMode>
--- src/pages/home/services/ServicesCard.js
+++ src/pages/home/services/ServicesCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
 
 const ServicesCard = ({service}) => {
     const { _id,img, price, name, description} = service
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img className='w-full h-100' src={img} alt="" /></figure>
+  <figure><PhotoView  src={img}>
+  <img className='w-full h-100' src={img} alt="" />
+    </PhotoView></figure>
   <div className="card-body">
     <h2 className="card-title">{name}</h2>
     <p>$ {price}</p>
